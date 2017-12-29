3b7f4b8eb05e4be0e4a615b8c945729178ce4c71
allenhu <allenhxn@sina.com>
2017-12-29 14:43:05 +0800

    update

--- lesson1/error/index2.php
+++ lesson1/error/index2.php
@@ -7,8 +7,8 @@
 
 function test(){
     try {
-        $a = 10 % 0;
-//        $b = 10 / 0;//这样捕捉不到error
+//        $a = 10 % 0;
+        $b = 10 / 0;    //这样捕捉不到error，后来发现产生的是警告，而不是抛出异常
         return 'Done';
     } catch (DivisionByZeroError $error) {
         return 'Error message：' . $error->getMessage();
